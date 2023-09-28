9c9ac96ab7690cc9abb89c3664cfb823a67538b2
ABBAS SAMI p2203929 <sami.abbas@etu.univ-lyon1.fr>
2023-09-28 16:54:50 +0000

    28/09/2023

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+<footer class="footer">
+  <div class="container">
+    <div class="footer-content">
+      <div class="footer-section">
+        <h2>À propos de nous</h2>
+        <p>Notre entreprise, fondée par deux étudiants en informatique à l'IUT de la Doua, est animée par notre passion commune pour la technologie et l'innovation. Nous nous efforçons de créer des solutions numériques créatives et efficaces pour répondre aux besoins de nos clients. Notre approche repose sur une combinaison de compétences techniques solides et d'une vision avant-gardiste, ce qui nous permet de développer des produits et des services qui repoussent les limites de la technologie tout en étant conviviaux et accessibles. Notre objectif est de fournir des solutions informatiques de qualité qui simplifient la vie de nos clients et les aident à atteindre leurs objectifs. Nous sommes impatients de collaborer avec vous pour transformer vos idées en réalité.</p>
+      </div>
+      <div class="footer-section">
+        <h2>Nos Services</h2>
+        <ul>
+          <li><a href="#">Service 1</a></li>
+          <li><a href="#">Service 2</a></li>
+          <li><a href="#">Service 3</a></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h2>Contact</h2>
+        <p>Contactez-nous pour plus d'informations :</p>
+        <p>Email : nenouscontacterpas@nefaispaschier.com</p>
+        <p>Téléphone : 06 66 66 66 66</p>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; 2023 Fil ami Blog. Tous droits réservés.</p>
+  </div>
+</footer>
+
+
+    </body>
+</html>
