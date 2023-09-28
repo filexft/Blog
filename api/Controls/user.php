612827c49c300623fb63a7ae80bb8971e71e0a5f
filexft <filimonteweldeft@gmail.com>
2023-09-28 22:27:19 +0200

    commit to change branch

---
+++ api/Controls/user.php
@@ -0,0 +1,22 @@
+
+class User{
+
+    $userid;
+    $db;
+    public function __construct(){
+        this->$db = new PDO('')
+    }
+
+    public function login(email, mdp){
+
+    }
+
+    
+    public function signup(){
+        
+    }
+
+    public function deleteArticle(){
+        
+    }
+}
