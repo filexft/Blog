4834c8ef48a0c7c6212f3bf7b20b9f819ac988a9
ABBAS SAMI p2203929 <sami.abbas@etu.univ-lyon1.fr>
2023-10-01 19:17:34 +0000

    01/10/2023

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <div class="footer-content">
       <div class="footer-section">
         <h2>À propos de nous</h2>
-        <p>Notre entreprise, fondée par deux étudiants en informatique à l'IUT de la Doua, est animée par notre passion commune pour la technologie et l'innovation. Nous nous efforçons de créer des solutions numériques créatives et efficaces pour répondre aux besoins de nos clients. Notre approche repose sur une combinaison de compétences techniques solides et d'une vision avant-gardiste, ce qui nous permet de développer des produits et des services qui repoussent les limites de la technologie tout en étant conviviaux et accessibles. Notre objectif est de fournir des solutions informatiques de qualité qui simplifient la vie de nos clients et les aident à atteindre leurs objectifs. Nous sommes impatients de collaborer avec vous pour transformer vos idées en réalité.</p>
+        <p>Notre entreprise, fondée par deux étudiants en informatique à l'IUT de la Doua. Nous nous efforçons de créer des solutions numériques créatives et efficaces pour répondre aux besoins de nos clients. Notre objectif est de fournir des solutions informatiques de qualité qui simplifient la vie de nos clients et les aident à atteindre leurs objectifs. </p>
       </div>
       <div class="footer-section">
         <h2>Nos Services</h2>
